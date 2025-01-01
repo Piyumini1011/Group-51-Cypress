9c9b8bdc26712eaa492af35cc600610a50825026
KusalAventureIT <161419904+KusalAventureIT@users.noreply.github.com>
2025-01-01 18:40:33 +0530

    Update check book list step 2

--- features/api/authentication.feature
+++ features/api/authentication.feature
@@ -1,15 +1,3 @@
-# @api
-# Feature: Authentication API Tests
-
-#   Scenario: Successfully access API with admin credentials
-#     When I request the list of books with username "admin" and password "password"
-#     Then I should get a response with status code 200
-
-#   Scenario: Successfully access API with user credentials
-#     When I request the list of books with username "user" and password "password"
-#     Then I should get a response with status code 200
-
-
 @api
 Feature: Authentication API Tests
 
--- features/api/steps/auth.steps.js
+++ features/api/steps/auth.steps.js
@@ -1,26 +1,3 @@
-// const { Given, When, Then } = require('@badeball/cypress-cucumber-preprocessor');
-
-// const API_URL = 'http://localhost:7081/api/books';
-
-// When("I request the list of books with username {string} and password {string}", function(username, password) {
-//   const credentials = `${username}, ${password}`;
-//   cy.request({
-//     method: 'GET',
-//     url: API_URL,
-//     headers: {
-//       "Content-Type": "application/json", // Set headers as per your API requirements
-//     },
-//     auth: {
-//       username: username,
-//       password: password},
-//     failOnStatusCode: true, // Allow handling of non-200 responses
-//   }).as('response');
-//   cy.wrap({
-//         username: username,
-//         password: password
-//     }).as('loginCredentials');
-// });
-
 const { Given, When, Then } = require('@badeball/cypress-cucumber-preprocessor');
 
 const API_URL = 'http://localhost:7081/api/books';
