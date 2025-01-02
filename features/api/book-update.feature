6a915935c04055390da74f40a73677a13b27867c
Imasha Rasangee <115669779+imasharasangee@users.noreply.github.com>
2025-01-02 12:50:42 +0530

    validating missing data for update

--- features/api/book-update.feature
+++ features/api/book-update.feature
@@ -19,3 +19,31 @@ Feature: Book Update API Tests
         | Updated Test Book  | Updated Author    |
         Then I should get a response with status code 403
         And response should contain message "User is not permitted."
+
+    Scenario: Missing author field
+        When I send POST request to create book with username "admin" and password "password"
+        | title          | author        |
+        | Test Book One  | Test Author 1 |
+        When I update the book with missing author field with username "admin" and password "password"
+        | title              |
+        | Updated Test Book  |
+        Then I should get a response with status code 400
+        And response should contain message "Mandatory parameters should not be null"
+      
+    Scenario: Missing title field
+        When I send POST request to create book with username "admin" and password "password"
+        | title          | author        |
+        | Test Book One  | Test Author 1 |
+        When I update the book with missing title field with username "admin" and password "password"
+        | author            |
+        | Updated Author    |
+        Then I should get a response with status code 400
+        And response should contain message "Mandatory parameters should not be null"
+    
+    Scenario: Empty request body
+        When I send POST request to create book with username "admin" and password "password"
+        | title          | author        |
+        | Test Book One  | Test Author 1 |
+        When I update the book with empty body with username "admin" and password "password"
+        Then I should get a response with status code 400
+        And response should contain message "Book id is not matched"
--- features/api/steps/updateBookSteps.js
+++ features/api/steps/updateBookSteps.js
@@ -1,5 +1,6 @@
 const { Given, When, Then } = require('@badeball/cypress-cucumber-preprocessor');
 
+const API_URL = 'http://localhost:7081/api/books';
 // Only keep the book-specific verification step
 Then("the book should be updated with new title and author", function() {
     cy.get('@response').then(response => {
@@ -20,4 +21,76 @@ Then("the book should be updated with new title and author", function() {
             });
         });
     });
+});
+
+When("I update the book with missing author field with username {string} and password {string}", function(username, password, dataTable) {
+  const updateData = dataTable.hashes()[0];
+  
+  // Add timestamp to make book data unique
+  const timestamp = new Date().getTime();
+  updateData.title = `${updateData.title}_${timestamp}`;
+  
+  cy.get('@bookId').then(bookId => {
+      // Add the book ID to the update data
+      updateData.id = bookId;
+      
+      cy.request({
+          method: 'PUT',
+          url: `${API_URL}/${bookId}`,
+          headers: {
+              "Content-Type": "application/json",
+          },
+          auth: {
+              username: username,
+              password: password
+          },
+          body: updateData,
+          failOnStatusCode: false
+      }).as('response');
+  });
+});
+
+When("I update the book with missing title field with username {string} and password {string}", function(username, password, dataTable) {
+  const updateData = dataTable.hashes()[0];
+  
+  // Add timestamp to make author data unique
+  const timestamp = new Date().getTime();
+  updateData.author = `${updateData.author}_${timestamp}`;
+  
+  cy.get('@bookId').then(bookId => {
+      // Add the book ID to the update data
+      updateData.id = bookId;
+      
+      cy.request({
+          method: 'PUT',
+          url: `${API_URL}/${bookId}`,
+          headers: {
+              "Content-Type": "application/json",
+          },
+          auth: {
+              username: username,
+              password: password
+          },
+          body: updateData,
+          failOnStatusCode: false
+      }).as('response');
+  });
+});
+
+When("I update the book with empty body with username {string} and password {string}", function(username, password) {
+  cy.get('@bookId').then(bookId => {
+      cy.request({
+          method: 'PUT',
+          url: `${API_URL}/${bookId}`,
+          headers: {
+              "Content-Type": "application/json",
+          },
+          auth: {
+              username: username,
+              password: password
+          },
+          body: {},  // Empty body
+          failOnStatusCode: false
+      }).as('response');
+  });
 });
